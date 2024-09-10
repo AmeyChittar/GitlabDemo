7c535a195a5035e473356a2ab6a0cb6c8d4cbb48
Amey Chittar <34574007+AmeyChittar@users.noreply.github.com>
2024-09-10 16:05:47 +0530

    my first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("This is a simple project to demonstrate git and gitlab");
+
+    }
+}
